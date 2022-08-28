f9f351c1fca6eaba9aa1430a6265a08e87e3e56a
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-08-27 22:41:31 -0700

    Change color, background, & border of nav bar when scrolled 	1. Use IntersectionObserver() to determine the intersection. 	2. Change the nav bar style when cross the intersection.

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+//*==================== Nav Bar Hide on Scrolling Down ====================*/
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = () => {
+//     let currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) 
+//     {
+//         document.getElementById("navbar").style.top = "0";
+//     } 
+//     else 
+//     {
+//         document.getElementById("navbar").style.top = "-124px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+// const navBar = document.querySelectorAll('navbar');
+
+
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".front__cover");
+
+const sectionOneOptions = {
+  rootMargin: "-300px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("navbar__scrolled");
+    } else {
+      header.classList.remove("navbar__scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+
 //*==================== Accordion Experience ====================*/
 const accordion = document.getElementsByClassName('accordion__item');
 
@@ -16,8 +58,8 @@ contact__toggle.onclick = function() {
 }
 
 // ==================== Circular Text ====================
-const text = document.querySelector(".circle__text p");
-text.innerHTML = text.innerText.split("").map(
-    (char, i) => 
-    `<span style="transform:rotate(${i*7.2}deg)">${char}</span>`
-).join("");
+// const text = document.querySelector(".circle__text p");
+// text.innerHTML = text.innerText.split("").map(
+//     (char, i) => 
+//     `<span style="transform:rotate(${i*7.2}deg)">${char}</span>`
+// ).join("");
