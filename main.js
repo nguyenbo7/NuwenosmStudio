7c90c5ae7981ead763e63a4b6d7301203b979ff4
Nguyen Chau <nguyenchaubo7@gmail.com>
2022-08-31 21:30:38 -0700

    Adding specific height for img in hero section

--- main.js
+++ main.js
@@ -75,18 +75,14 @@ contact__toggle.onclick = function() {
 //     },
 // });
 let swiper = new Swiper(".studio__imgs-container", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
     },
   });
 
